1a1270d075c866928ec93422c947656fe26dc082
Antonino Scaffidi <antoninoscaffidi@gmail.com>
2016-08-05 14:13:52 +0200

    Added static lesson resource

---
+++ app/controllers/lessons_controller.rb
@@ -0,0 +1,7 @@
+class LessonsController < ApplicationController
+  def il_linguaggio
+  end
+
+  def struttura_documento
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
 
+  get 'lessons/il-linguaggio'
+
+  get 'lessons/struttura-documento'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "home#index"
   match 'corsi/html5'=> 'courses#html5', via: :get
+  match 'corsi/html5/il-linguaggio' => 'lessons#il_linguaggio', via: :get
+  match 'corsi/html5/struttura-documento' => 'lessons#struttura_documento', via: :get
+ 
 end
