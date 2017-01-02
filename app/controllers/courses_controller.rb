6a348a865b9a315c50067e1dbd180852abe5348a
Antonino Scaffidi <antoninoscaffidi@gmail.com>
2017-01-02 10:01:56 +0100

    Added action index in courses controller

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,4 +1,10 @@
 class CoursesController < ApplicationController
+  
+  def index
+    @courses = [ 'Html5', 'Css3', 'Javascript', 'Ruby On Rails', 'Ruby'] 
+  end
+  
+  
   def html5
     @course = "Html5"
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+    
+  get "courses" => "courses#index"
+
+    
 
   get 'exercises/index'
 
