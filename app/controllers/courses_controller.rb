5b62042322ed98bf42eb477c7e3e8ac258df0148
Antonino Scaffidi <antoninoscaffidi@gmail.com>
2017-01-10 06:51:16 +0100

    Actived Ruby On Rails course

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,11 +1,16 @@
 class CoursesController < ApplicationController
-  
+
   def index
-    @courses = [ 'Html5', 'Css3', 'Javascript', 'Ruby On Rails', 'Ruby'] 
+    @courses = [ 'Html5', 'Css3', 'Javascript', 'Ruby On Rails', 'Ruby']
   end
-  
-  
+
+
   def html5
     @course = "Html5"
   end
+
+  def ruby_on_rails
+    @course = "Ruby on Rails"
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-    
+
   get "courses" => "courses#index"
 
-    
+
 
   get 'exercises/index'
 
@@ -11,13 +11,17 @@ Rails.application.routes.draw do
   get 'lessons/il-linguaggio'
 
   get 'lessons/struttura-documento'
-  
+
   get 'lessons/protocollo-http'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "home#index"
   match 'corsi/html5'=> 'courses#html5', via: :get
+  match 'corsi/ruby-on-rails'=> 'courses#ruby_on_rails', via: :get
+  match 'corsi/ruby-on-rails/il-framework-ruby-on-rails' => 'lessons#il_framework_ruby_on_rails', via: :get
+  match 'corsi/ruby-on-rails/introduzione' => 'lessons#introduzione', via: :get
+  match 'corsi/ruby-on-rails/pattern-mvc' => 'lessons#pattern_mvc', via: :get
   match 'corsi/html5/il-linguaggio' => 'lessons#il_linguaggio', via: :get
   match 'corsi/html5/struttura-documento' => 'lessons#struttura_documento', via: :get
-  match 'corsi/html5/esercizi/hello-world' => 'exercises#html5_hello_world', via: :get 
+  match 'corsi/html5/esercizi/hello-world' => 'exercises#html5_hello_world', via: :get
 end
